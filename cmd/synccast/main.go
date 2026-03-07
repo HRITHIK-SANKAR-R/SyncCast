d961d7128a923dd53f6977d3c5c417462a27480f
HRITHIK-SANKAR-R <hrithiksrr@gmail.com>
2026-03-07 12:54:05 +0530

    refactor: simplify main.go output

--- cmd/synccast/main.go
+++ cmd/synccast/main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
-
 	"github.com/HRITHIK-SANKAR-R/SyncCast/internal/discovery"
 )
 
 func main() {
-	fmt.Println("Synccast : Initialising")
+	fmt.Println("SyncCast: Discovering Android TVs...")
 	
-	ip, err := discovery.GetLocalIP(); 
-	if err != nil {
-		log.Fatal(err)
+	devices := discovery.DiscoverAllTVs()
+	
+	if len(devices) == 0 {
+		fmt.Println("No TVs found")
+		return
 	}
-	fmt.Printf("Current ip of the wifi: %s\n", ip)
 	
+	fmt.Printf("\nFound %d device(s):\n", len(devices))
+	for i, d := range devices {
+		fmt.Printf("%d. %s (%s)\n", i+1, d.Name, d.IP)
+	}
 }
